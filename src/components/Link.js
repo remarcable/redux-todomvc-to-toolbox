c41ea2ab1fa4530c1deec004ab0229f2e27ab79c
Marc N <git@marcnitzsche.de>
2022-04-01 16:22:59 +0200

    Add prettier

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 
-const Link = ({ active, children, setFilter }) =>
-  (
+const Link = ({ active, children, setFilter }) => (
     // eslint-disable jsx-a11y/anchor-is-valid
     <a
-      className={classnames({ selected: active })}
-      style={{ cursor: 'pointer' }}
-      onClick={() => setFilter()}
+        className={classnames({ selected: active })}
+        style={{ cursor: "pointer" }}
+        onClick={() => setFilter()}
     >
-      {children}
+        {children}
     </a>
-  )
-
+);
 
 Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  setFilter: PropTypes.func.isRequired
-}
+    active: PropTypes.bool.isRequired,
+    children: PropTypes.node.isRequired,
+    setFilter: PropTypes.func.isRequired,
+};
 
-export default Link
+export default Link;
